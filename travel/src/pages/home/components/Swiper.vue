bb4579a1084331bfe87dfe0eae2931b616971e5a
eson <834767372@qq.com>
2021-01-08 09:15:53 +0800

    【更新】- 7-3 Vue项目首页 - 首页轮播图

---
+++ travel/src/pages/home/components/Swiper.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption" ref="mySwiper">
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" :alt="item.id">
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '1',
+        imgUrl: 'https://tr-osdcp.qunarzz.com/tr-osd-tr-manager/img/f120bd9310af9b72bbac8d005f1ef146.jpg'
+      }, {
+        id: '2',
+        imgUrl: 'https://tr-osdcp.qunarzz.com/tr-osd-tr-manager/img/baa26b15ba14d443feeb78df47160524.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background #fff
+  .wrapper
+    overflow hidden
+    width 100%
+    height 0
+    padding-bottom 53.33%
+    background-color: #eee;
+    .swiper-img
+      width 100%
+</style>
